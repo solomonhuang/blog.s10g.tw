33d237329de47d9364dbdb215338206b2a6e8fb1
Solomon Huang <kaichanh@gmail.com>
2016-06-17 03:16:46 +0800

    migrate to pelican

---
+++ content/2012-12-21-gmail-pop3s-and-self-signed-ca.markdown
@@ -0,0 +1,17 @@
+---
+layout: post
+title: "GMail 不支援自行簽署的憑證"
+date: 2012-12-21 11:10
+slug: gmail-pop3s-and-self-signed-ca
+comments: true
+categories: [gmail, pop3, ssl]
+---
+
+從 12 月 13 日開始，GMail 已經不支援自己簽發的憑證。為了安全性，但是很不方便。
+
+從 2012/12/06 更新的 [官方說明][gmail] 可以看到。
+
+> 我們不接受自行簽署的憑證。有效的憑證必須連結至有效的 CA，例如「Mozilla CA 清單」。
+
+[gmail]: https://support.google.com/mail/bin/answer.py?hl=zh-Hant&answer=21291 "擷取郵件時一律使用安全連線 (SSL)"
+
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Solomon Huang'
+SITENAME = '所羅門筆記本'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = 'zh'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = ()
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
